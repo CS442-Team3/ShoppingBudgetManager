aba9e24f93b2cf8eda0e1285b78f1e98c1397c9c
karanjeetsingh <karanjeetsingh@live.in>
2014-10-18 21:09:23 -0500

    Added budget_edit and shopping list details_fragment
    i.e. screen 7 and 8

--- gen/com/cs442team3/shoppingbudgetmanager/R.java
+++ gen/com/cs442team3/shoppingbudgetmanager/R.java
@@ -8,6 +8,9 @@
 package com.cs442team3.shoppingbudgetmanager;
 
 public final class R {
+    public static final class array {
+        public static final int duration_spinner_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,21 +25,76 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int delete_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f09002e;
+        public static final int amount_et=0x7f090001;
+        public static final int amount_rl=0x7f090000;
+        public static final int btns_view=0x7f09002c;
+        public static final int continue_btn=0x7f090009;
+        public static final int continue_rl=0x7f090008;
+        public static final int details_frgmnt_amount_tv=0x7f090010;
+        public static final int details_frgmnt_amount_tv1=0x7f09000d;
+        public static final int details_frgmnt_amount_tv2=0x7f090014;
+        public static final int details_frgmnt_amount_tv3=0x7f090018;
+        public static final int details_frgmnt_amount_tv4=0x7f09001c;
+        public static final int details_frgmnt_amount_tv5=0x7f090020;
+        public static final int details_frgmnt_amount_tv6=0x7f090024;
+        public static final int details_frgmnt_amount_tv7=0x7f090028;
+        public static final int details_frgmnt_btns_rl=0x7f09002a;
+        public static final int details_frgmnt_date_tv=0x7f09000f;
+        public static final int details_frgmnt_date_tv1=0x7f09000c;
+        public static final int details_frgmnt_date_tv2=0x7f090013;
+        public static final int details_frgmnt_date_tv3=0x7f090017;
+        public static final int details_frgmnt_date_tv4=0x7f09001b;
+        public static final int details_frgmnt_date_tv5=0x7f09001f;
+        public static final int details_frgmnt_date_tv6=0x7f090023;
+        public static final int details_frgmnt_date_tv7=0x7f090027;
+        public static final int details_frgmnt_entermanually_btn=0x7f09002d;
+        public static final int details_frgmnt_iv=0x7f090011;
+        public static final int details_frgmnt_iv2=0x7f090015;
+        public static final int details_frgmnt_iv3=0x7f090019;
+        public static final int details_frgmnt_iv4=0x7f09001d;
+        public static final int details_frgmnt_iv5=0x7f090021;
+        public static final int details_frgmnt_iv6=0x7f090025;
+        public static final int details_frgmnt_iv7=0x7f090029;
+        public static final int details_frgmnt_month_tv=0x7f09000a;
+        public static final int details_frgmnt_rl=0x7f09000e;
+        public static final int details_frgmnt_rl1=0x7f09000b;
+        public static final int details_frgmnt_rl2=0x7f090012;
+        public static final int details_frgmnt_rl3=0x7f090016;
+        public static final int details_frgmnt_rl4=0x7f09001a;
+        public static final int details_frgmnt_rl5=0x7f09001e;
+        public static final int details_frgmnt_rl6=0x7f090022;
+        public static final int details_frgmnt_rl7=0x7f090026;
+        public static final int details_frgmnt_scanbarcode_btn=0x7f09002b;
+        public static final int duration_radio=0x7f090005;
+        public static final int duration_radio_rl=0x7f090004;
+        public static final int duration_spinner=0x7f090003;
+        public static final int duration_spinner_rl=0x7f090002;
+        public static final int radio_1month=0x7f090007;
+        public static final int radio_till30=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int budgetedit_activity=0x7f030001;
+        public static final int details_fragment=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int details=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
+        public static final int _1month=0x7f050006;
+        public static final int _continue=0x7f050004;
         public static final int action_settings=0x7f050002;
+        public static final int amount_tv=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int till30=0x7f050005;
+        public static final int title_activity_details_fragment=0x7f050007;
     }
     public static final class style {
         /** 
@@ -60,10 +118,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
